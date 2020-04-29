aeb373168b6e83b6c4fae480c60b824c197774dc
07smithlewis <57406097+07smithlewis@users.noreply.github.com>
2020-04-29 19:49:24 +0100

    fix

--- js/jsonLoader.js
+++ js/jsonLoader.js
@@ -1,5 +1,6 @@
 function jsonLoader(file) {
   var json = undefined;
+  var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       json = JSON.parse(this.responseText);
